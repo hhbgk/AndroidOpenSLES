1bbda79ef2fd42821f7668a3f1b7ad0ef8f853b1
hhbgk <hhbgke@gmail.com>
2018-08-13 19:42:02 +0800

    Initial commit

---
+++ app/src/main/java/com/hhbgk/example/opensles/OpenSlWrapper.java
@@ -0,0 +1,19 @@
+package com.hhbgk.example.opensles;
+
+/**
+ * Description:
+ * Author:created by bob on 18-8-13.
+ */
+public final class OpenSlWrapper {
+    private native boolean nativeStartCapture();
+
+    private native boolean nativeStopCapture();
+
+    private native boolean nativeStartPlayback();
+
+    private native boolean nativeStopPlayback();
+
+    static {
+        System.loadLibrary("native-lib");
+    }
+}
